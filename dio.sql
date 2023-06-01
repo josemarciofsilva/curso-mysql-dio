de78e5ba10e07a52c58abe434dea80c01eec9842
Jose Marcio Ferreira da Silva <jmarciosilva@gmail.com>
2023-06-01 20:36:01 -0300

    Comando DELETE com clausula WHERE

--- dio.sql
+++ dio.sql
@@ -2,7 +2,7 @@ CREATE TABLE pessoas (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50)NOT NULL,
     nascimento DATE
-)
+);
 
 INSERT INTO pessoas (nome, nascimento) VALUES('José Marcio', '1975-01-20');
 INSERT INTO pessoas (nome, nascimento) VALUES('Aparecida', '1947-12-04');
@@ -10,3 +10,13 @@ INSERT INTO pessoas (nome, nascimento) VALUES('Livia Ferreira', '2006-11-13');
 
 UPDATE pessoas set nome='Aparecida Cardoso da Silva' WHERE id = 2; 
 UPDATE pessoas set nome='Livia Jussara Ferreira' WHERE id = 3; 
+
+INSERT INTO pessoas (nome, nascimento) VALUES('Pedrinho Maluquino', '2006-11-13');
+
+SELECT * FROM pessoas;
+
+DELETE FROM pessoas WHERE nome = 'Pedrinho Maluquino';
+
+SELECT * FROM pessoas;
+
+
