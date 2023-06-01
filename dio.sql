f477cdc51c4b847f8c3e734ae496c69f604307aa
Jose Marcio Ferreira da Silva <jmarciosilva@gmail.com>
2023-06-01 20:20:33 -0300

    Comando UPDATE com clausula WHERE

--- dio.sql
+++ dio.sql
@@ -7,3 +7,6 @@ CREATE TABLE pessoas (
 INSERT INTO pessoas (nome, nascimento) VALUES('José Marcio', '1975-01-20');
 INSERT INTO pessoas (nome, nascimento) VALUES('Aparecida', '1947-12-04');
 INSERT INTO pessoas (nome, nascimento) VALUES('Livia Ferreira', '2006-11-13');
+
+UPDATE pessoas set nome='Aparecida Cardoso da Silva' WHERE id = 2; 
+UPDATE pessoas set nome='Livia Jussara Ferreira' WHERE id = 3; 
